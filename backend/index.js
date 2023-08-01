e020585ce82d1f8f27df9948d57e3d2e64a8a963
Het Patel <106577127+Het01@users.noreply.github.com>
2023-08-01 23:01:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,14 @@ const corsOptions ={
 const mongoDB = require('./db');
 mongoDB();
 
-// app.use((req,res,next)=>{
-//   res.setHeader("Access-Control-Allow-Origin", "https://hp-electronics.vercel.app");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// })
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "https://hp-electronics.vercel.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 app.use(express.json())
 app.use(cors(corsOptions))
